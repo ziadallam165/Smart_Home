f2d6f5a8c6daf434fc8aee8e1fe3ccedc322c1c6
ziadallam165 <135058996+ziadallam165@users.noreply.github.com>
2023-09-10 23:15:33 +0300

    Add files via upload

---
+++ Stepper_Motor/STEPPER.c
@@ -0,0 +1,45 @@
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "DIO.h"
+#include "avr/io.h"
+#include "util/delay.h"
+#include "STEPPER.h"
+
+
+
+void Stepper_Init(void){
+	
+	DIO_SetPinDirection(DIO_PORTA,DIO_PIN4,DIO_OUTPUT);
+	DIO_SetPinDirection(DIO_PORTA,DIO_PIN5,DIO_OUTPUT);
+	DIO_SetPinDirection(DIO_PORTA,DIO_PIN6,DIO_OUTPUT);
+	DIO_SetPinDirection(DIO_PORTA,DIO_PIN7,DIO_OUTPUT);
+	
+	
+}
+
+void Stepper_Open_90deg(void){
+	
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN4, DIO_LOW);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN6, DIO_LOW);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN7, DIO_LOW);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN5, DIO_HIGH);
+		
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN4, DIO_LOW);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN6, DIO_HIGH);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN7, DIO_LOW);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN5, DIO_HIGH);
+
+		_delay_ms(100);	
+	
+}
+void Stepper_Close_door(void){
+		
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN4, DIO_HIGH);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN5, DIO_HIGH);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN7, DIO_LOW);
+		DIO_SetPinValue(DIO_PORTA, DIO_PIN6, DIO_LOW);
+
+		_delay_ms(100);
+	
+}
---
+++ Stepper_Motor/STEPPER.h
@@ -0,0 +1,11 @@
+#ifndef STEPPER_H_
+#define STEPPER_H_
+
+void Stepper_Init(void);
+void Stepper_Open_90deg(void);
+void Stepper_Close_door(void);
+
+
+
+
+#endif
